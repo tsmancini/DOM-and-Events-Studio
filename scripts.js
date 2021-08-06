3ae105abcbb511ad628aeaf512bf8ac908ac1d6d
Tim Davis <tsmancini@aol.com>
2021-08-05 20:13:23 -0400

    Added taKeoff button clicked functions. Started landing button click functions

--- scripts.js
+++ scripts.js
@@ -1,2 +1,36 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+
+
+
+
+function init () {
+ const takeoffButtonClicked = document.getElementById("takeoff");
+ function takeoffButtonClickedfunction() {
+     const answer = confirm('Confirm that the shuttle is ready for takeoff.');
+     if (answer === true) {
+        const flightStatus = document.getElementById("flightStatus");
+        flightStatus.innerText = "Shuttle in flight.";
+        const shuttleBackground = document.getElementById("shuttleBackground");
+        shuttleBackground.style.background = "blue";
+        const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+        spaceShuttleHeight.innerText = Number(spaceShuttleHeight.innerText) + 10000;
+     }
+    const landButtonClicked = document.getElementById("landing");
+    function landButtonClicked () {
+        alert("The shuttle is landing. Landing gear engaged.");
+
+    }
+    
+
+
+ }
+
+ takeoffButtonClicked.addEventListener('click', takeoffButtonClickedfunction);
+ landButtonClicked.addEventListener('click', landButtonClicked);
+
+
+}
+
+window.addEventListener("load", init);
